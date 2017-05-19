f9c75f53a4abe5b41d4fa7a36bdb2747d85435f7
jpausic <jpausic@172.18.2.253>
2017-05-19 14:13:16 -0400

    testing with eclipse

--- src/Card.java
+++ src/Card.java
@@ -3,7 +3,7 @@ public class Card extends PokemonGame {
 	static Type type;
 	public static void pokemonConstructor(){
 		type = Type.POKEMON;
-		//changes
+		//change
 	}
 	public static void trainingConstructor(){
 		type = Type.TRAINER;
