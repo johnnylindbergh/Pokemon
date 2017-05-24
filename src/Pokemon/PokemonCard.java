ac9b0a59d9a207789f365c29065e997f054d00d9
jpausic <jpausic@172.18.2.253>
2017-05-24 14:28:04 -0400

    work

--- src/Pokemon/PokemonCard.java
+++ src/Pokemon/PokemonCard.java
@@ -1,10 +1,11 @@
 package Pokemon;
 public class PokemonCard extends Card{
 	int hp = 0;
+	// int hp_max = ??
 	int attack = 0;
 	public enum Type{FIRE, WATER, GRASS};
 	static Type type;
-	public pokemon(){
+	public void pokemon(){
 	}
 	
 }
